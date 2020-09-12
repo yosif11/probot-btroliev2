53e94da4082eab3e127846dccadfddbe06e1be38
yosif11 <49305870+yosif11@users.noreply.github.com>
2020-09-12 07:43:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.listen(() => console.log("start btrolie"));
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+
+
+
+
+
 const Discord = require('discord.js');
 const moment = require("moment");  
 const fs = require("fs");      
